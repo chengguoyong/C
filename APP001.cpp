0b24c8559a31799293c6499eac19318f2c02d8d6
unknown <guoyong_cheng@SOG.COM>
2016-12-07 11:09:27 +0800

    Signed-off-by: unknown <guoyong_cheng@SOG.COM>

---
+++ APP001.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+ using namespace std;
+ void fibonacci (int g)
+  {
+ 	int i,a,b,c,iNumber;
+  	a=1;
+  	b=1;
+ 	if (iNumber < 3)
+ 	cout << "Please input Fibonacci Number:" ;
+ 	cin >> iNumber;
+ 	if (iNumber < 3)
+  		cout << "Error, input must > 3!";
+  	else
+  	{ 	cout << a <<" "<< b << " ";
+ 		for (i=3;i<=iNumber;i++)
+  		{	
+  			c = b;
+  			b = a + b;
+  			a = c;
+  			cout << b << " ";
+  		}
+  	}; 
+ 	cout << endl;
+  }	 
+  main()
+  {
+ 	char cSelect;
+ 	cout << "Please select" << endl;
+ 	cout << "A Fibonacci" << endl;
+ 	cout << "B " << endl; 
+ 	cout << "0 Exit" << endl;
+ 	cin >> cSelect;
+ 	switch (cSelect)
+ 	{
+ 		case 'A': fibonacci(4);
+ 	};
+  }
+
