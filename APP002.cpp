23ac6d60891523d6de523a01bcb8ea1afc2d83ca
guoyong.cheng <chenggy@163.com>
2016-12-09 15:39:58 +0800

    Add two program

---
+++ APP002.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+main()
+{
+	double dLength,dWidth,dHigh;
+	cout << "Please input Length Width and High" << endl;
+	cout << "Length = ";
+	cin >> dLength;
+	cout << endl << "Width = ";
+	cin >> dWidth;
+	cout << endl << "High = ";
+	cin >> dHigh;
+	
+	cout << endl << "The volumn of cube is " << setiosflags(ios::fixed) << setprecision(2) << dLength * dWidth * dHigh << endl;
+}
---
+++ APP003.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+main()
+{
+	double dR, dPie;
+	cout << "Please input the radius and Pie " << endl << "Radius = ";
+	cin >> dR;
+	cout << "Pie = ";
+	cin >> dPie;
+	cout << "The Perimeter of Round is " << setiosflags(ios::fixed) << setprecision(2) << dPie * dR * 2 << endl;
+	cout << "The Area of Round is " << setiosflags(ios::fixed) << setprecision(2) << dPie * dR * dR << endl;
+ } 
